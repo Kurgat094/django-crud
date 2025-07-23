ec23d446f7f13113df6a6e90b19d1bb6ad27579f
Kurgat094 <tobiaskipkogei@gmail.com>
2025-07-23 13:07:07 +0300

    CRSF_EXEMPT

--- Todo/views.py
+++ Todo/views.py
@@ -1,26 +1,28 @@
 from django.shortcuts import render, redirect
 from .models import Todo
 from django.contrib import messages
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def home(request):
     return render(request,'index.html')
-
+@csrf_exempt
 def view(request):
     todos = Todo.objects.all()
     return render(request, 'view.html', {'todos': todos})
 
+@csrf_exempt
 def add(request):
     if request.method == 'POST':
         name = request.POST.get('name')
         description = request.POST.get('description')
-        completed = request.POST.get('completed')
+        completed = request.POST.get('completed','no')
         todo = Todo(name=name, description=description, completed=completed)
         todo.save()
         messages.success(request, 'Todo added successfully')
         return redirect('view')
     return render(request,'add.html')
-
+@csrf_exempt
 def edit(request,pk):
     todo = Todo.objects.get(pk=pk)
     if request.method == 'POST':
@@ -31,7 +33,7 @@ def edit(request,pk):
         messages.success(request, 'Todo updated successfully')
         return redirect('view')
     return render(request,'edit.html', {'todo': todo})
-
+@csrf_exempt
 def delete(request, pk):
     todo = Todo.objects.get(pk=pk)
     todo.delete()
